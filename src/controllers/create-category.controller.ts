3cfcb7eb28a9cf4087573e4d59f2be8d1f7a02c1
Adriano <adrianoact@msn.com>
2024-12-18 21:35:10 -0300

    Create category controller

---
+++ src/controllers/create-category.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createCategoryBodySchema = z.object({
+  title: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createCategoryBodySchema)
+
+type CreateCategoryBodySchema = z.infer<typeof createCategoryBodySchema>
+
+@Controller('/categories')
+@UseGuards(JwtAuthGuard)
+export class CreateCategoryController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(@Body(bodyValidationPipe) body: CreateCategoryBodySchema) {
+    const { title } = body
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.category.create({
+      data: {
+        title,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+  }
+}
