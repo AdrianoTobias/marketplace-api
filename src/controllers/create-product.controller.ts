14d1c41ba645a159d93a0287aed3582d24ce33ec
Adriano <adrianoact@msn.com>
2024-12-18 21:35:28 -0300

    Create product controller

--- src/controllers/create-product.controller.ts
+++ src/controllers/create-product.controller.ts
@@ -1,17 +1,51 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createProductBodySchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  priceInCents: z.number(),
+  categoryId: z.string().uuid(),
+  attachmentsIds: z.array(z.string().uuid()),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createProductBodySchema)
+
+type CreateProductBodySchema = z.infer<typeof createProductBodySchema>
 
 @Controller('/products')
 @UseGuards(JwtAuthGuard)
 export class CreateProductController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub)
+  async handle(
+    @Body(bodyValidationPipe) body: CreateProductBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, categoryId, description, priceInCents, attachmentsIds } =
+      body
+    const userId = user.sub
+    const status = 'available'
 
-    return 'ok'
+    await this.prisma.product.create({
+      data: {
+        ownerId: userId,
+        title,
+        description,
+        priceInCents,
+        status,
+        categoryId,
+        // attachments: attachmentsIds,
+        attachments: {
+          connect: attachmentsIds.map((id) => ({ id })),
+        },
+      },
+    })
   }
 }
