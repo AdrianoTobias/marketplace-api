a0a81dff738f7f54b52fb91445635c7c338600d2
Adriano <adrianoact@msn.com>
2024-12-28 20:58:14 -0300

    Create env module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
+import { envSchema } from './env/env'
 import { AuthModule } from './auth/auth.module'
 import { httpModule } from './http/http.module'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { httpModule } from './http/http.module'
     }),
     AuthModule,
     httpModule,
+    EnvModule,
   ],
 })
 export class AppModule {}
