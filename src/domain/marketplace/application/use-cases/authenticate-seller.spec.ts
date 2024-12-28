d651de81dc744c655eb825f3982d719789cb044b
Adriano <adrianoact@msn.com>
2024-12-28 17:53:36 -0300

    Authenticate seller use case test

---
+++ src/domain/marketplace/application/use-cases/authenticate-seller.spec.ts
@@ -0,0 +1,79 @@
+import { InMemorySellersRepository } from 'test/repositories/in-memory-sellers-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { AuthenticateSellerUseCase } from './authenticate-seller'
+import { makeSeller } from 'test/factories/make-seller'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+let inMemorySellersRepository: InMemorySellersRepository
+let fakeHasher: FakeHasher
+let encrypter: FakeEncrypter
+
+let sut: AuthenticateSellerUseCase
+
+describe('Authenticate Seller', () => {
+  beforeEach(() => {
+    inMemorySellersRepository = new InMemorySellersRepository()
+    fakeHasher = new FakeHasher()
+    encrypter = new FakeEncrypter()
+
+    sut = new AuthenticateSellerUseCase(
+      inMemorySellersRepository,
+      fakeHasher,
+      encrypter,
+    )
+  })
+
+  it('should be able to authenticate a seller', async () => {
+    const seller = makeSeller({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemorySellersRepository.items.push(seller)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+
+  it('should not be able to authenticate a seller with wrong email', async () => {
+    const seller = makeSeller({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemorySellersRepository.items.push(seller)
+
+    const result = await sut.execute({
+      email: 'wrong@email.com',
+      password: '123456',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(WrongCredentialsError)
+  })
+
+  it('should not be able to authenticate a seller with wrong password', async () => {
+    const seller = makeSeller({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemorySellersRepository.items.push(seller)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(WrongCredentialsError)
+  })
+})
